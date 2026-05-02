63ff85ce5a415d54fc8cff2aa1c601a17c47fae7
OnlyVCS <vcsdevacc@gmail.com>
2026-05-01 21:07:41 -0300

    0.5.0 - feat: adiciona novas rotas para páginas de exploração e usuário no App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/Home/HomePage'
-import LoginPage from './pages/Login/LoginPage'
-import AlbumPage from './pages/Album/AlbumPage'
+import HomePage        from './pages/Home/HomePage'
+import LoginPage       from './pages/Login/LoginPage'
+import AlbumPage       from './pages/Album/AlbumPage'
+import ExplorePage     from './pages/Explore/ExplorePage'
+import UserPage        from './pages/User/UserPage'
+import EditProfilePage from './pages/EditProfile/EditProfilePage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"          element={<HomePage />}  />
-        <Route path="/login"     element={<LoginPage />} />
-        <Route path="/album/:id" element={<AlbumPage />} />
+        <Route path="/"                    element={<HomePage />}        />
+        <Route path="/login"               element={<LoginPage />}       />
+        <Route path="/album/:id"           element={<AlbumPage />}       />
+        <Route path="/explorar"            element={<ExplorePage />}     />
+        <Route path="/usuario/:handle"     element={<UserPage />}        />
+        <Route path="/usuario/editar"      element={<EditProfilePage />} />
       </Routes>
     </BrowserRouter>
   )
